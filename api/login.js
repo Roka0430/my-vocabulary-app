d4ad04562c707ca89bfc1de399fcbc9cfb9db40c
Roka0430 <noxx0430@gmail.com>
2026-01-31 08:25:39 +0900

    認証画面

--- api/login.js
+++ api/login.js
@@ -1,6 +1,12 @@
 export default function handler(req, res) {
-  const { pass } = req.query;
-  if (pass == process.env.APP_PASSWORD) {
-    return res.status(200).send("<h1>認証成功</h1>");
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Found");
+  }
+
+  const { pass } = req.body;
+  if (pass === process.env.APP_PASSWORD) {
+    res.redirect(302, "/main.html");
+  } else {
+    res.status(401).send("<h1>パスワードが異なります。</h1><a href='/'>戻る</a>");
   }
 }
