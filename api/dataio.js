fd59f1447049272de7b6e00dba19e02eb512c1e3
Roka0430 <noxx0430@gmail.com>
2026-01-31 09:38:47 +0900

    json新たに

--- api/dataio.js
+++ api/dataio.js
@@ -1,3 +1,12 @@
+import path from "path";
+import { promises as fs } from "fs";
+
 export default async function handler(req, res) {
-  res.status(200).send("OK");
+  try {
+    const filename = path.join(process.cwd(), "resource", "vocabulary.json");
+    const contents = await fs.readFile(filename, "utf8");
+    res.status(200).json(JSON.parse(contents));
+  } catch (error) {
+    res.status(500).json({ error: "ファイルの読み込みに失敗しました。" });
+  }
 }
