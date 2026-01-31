507a495dbaffd8e0962576207ac192410d1ddd94
Roka0430 <noxx0430@gmail.com>
2026-01-31 09:01:37 +0900

    検証もう一度

--- api/check.js
+++ api/check.js
@@ -4,11 +4,12 @@ import path from "path";
 export default function handler(req, res) {
   const cookieHeader = req.headers.cookie || "";
   const cookies = Object.fromEntries(cookieHeader.split("; ").map((c) => c.split("=")));
+  console.log(cookies);
 
   const token = cookies["login_token"];
+  console.log(token);
 
   if (token === "success") {
-    // 認証OK：publicフォルダのmain.htmlを読み込んで返す
     const filePath = path.join(process.cwd(), "public", "main.html");
     const fileContent = fs.readFileSync(filePath, "utf8");
     res.setHeader("Content-Type", "text/html");
