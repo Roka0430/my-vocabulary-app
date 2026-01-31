ae90954bb2b655c14d32c910df103755c69a926a
Roka0430 <noxx0430@gmail.com>
2026-01-31 09:36:25 +0900

    json検証

--- api/dataio.js
+++ api/dataio.js
@@ -1,8 +1,3 @@
-import { promises as fs } from "fs";
-import path from "path";
-
-export default async function handler(request, response) {
-  const resourceDir = path.join(process.cwd(), "resource");
-  const fileContents = await fs.readFile(resourceDir + "/data.json", "utf8");
-  response.status(200).json(JSON.parse(fileContents));
+export default async function handler(req, res) {
+  res.status(200).send("OK");
 }
