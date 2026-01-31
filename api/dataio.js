97e913ff8643ea21f6fd5803e13ffde48e090706
Roka0430 <noxx0430@gmail.com>
2026-01-31 20:30:02 +0900

    update

--- api/dataio.js
+++ api/dataio.js
@@ -3,9 +3,19 @@ import { promises as fs } from "fs";
 
 export default async function handler(req, res) {
   try {
+    const { startend, length } = req.query;
+
     const filename = path.join(process.cwd(), "resource", "vocabulary.json");
     const contents = await fs.readFile(filename, "utf8");
-    res.status(200).json(JSON.parse(contents));
+
+    if (startend) {
+      const [start, end] = startend.split("-");
+      res.status(200).json(JSON.parse(contents).slice(start - 1, end));
+    } else if (length) {
+      res.status(200).json({ len: JSON.parse(contents).length });
+    } else {
+      res.status(200).json(JSON.parse(contents));
+    }
   } catch (error) {
     res.status(500).json({ error: "ファイルの読み取りに失敗しました。" });
   }
